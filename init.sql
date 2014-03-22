04cda98220acd836dc7350e8130607f0716c37e1
Daniel <daniel@steinnagel.se>
2014-03-22 11:42:54 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,26 +1,69 @@
-Ingridients(Ingredient, Quantity, Unit)
-Ingredient → Quantity, Unit
+CREATE TABLE Ingredients
+(
+Ingredient VARCHAR(100) PRIMARY KEY,
+Quantity INT NOT NULL CHECK (Quantity>=0),
+Unit VARCHAR(10) NOT NULL
+);
 
-IngredientDelivery(DeliveryID, Ingredient, Date, Quantity)
-DeliveryID -> Ingredient, Date, Quantity
+CREATE TABLE IngredientDelivery 
+(
+DeliveryID INT auto_increment PRIMARY KEY,
+Ingredient VARCHAR(100) REFERENCES Ingredients(Ingredient),
+Date datetime NOT NULL DEFAULT NOW(),
+Quantity INT NOT NULL CHECK (Quantity>0)
 
-Recipes(ProductName)
+);
 
-RecipieIngredients(ProductName, Ingredient, Quantity)
-ProductName, Ingredient → Quantity
+CREATE TABLE Recipes
+(
+ProductName VARCHAR(100) PRIMARY KEY;
+);
 
-Pallets(PalletID, ProductName, Blocked, ProductionDate)
-PalletID → ProductName, Blocked, ProductionDate
+CREATE TABLE RecipeIngredients 
+(
+ProductName VARCHAR(100) REFERENCES Recipes(ProductName),
+Ingredient VARCHAR(100) REFERENCES Ingridients(Ingredient),
+Quantity INT NOT NULL CHECK (Quantity>0),
+PRIMARY KEY (ProductName, Ingredient)
+);
 
-Storage(PalletID)
+CREATE TABLE Pallets 
+(
+PalletID INT auto_increment PRIMARY KEY,
+ProductName VARCHAR(100) REFERENCES Recipes(ProductName),
+Blocked BOOL NOT NULL DEFAULT 0,
+ProductionDate datetime NOT NULL DEFAULT NOW()
+);
 
-Orders(OrderID, Customer)
-OrderID → Customer
+CREATE TABLE Storage
+(
+PalletId INT REFERENCES(Pallets(PalletID)) PRIMARY KEY,
+);
 
-PartialOrders(OrderID, ProductName, Quantity)
-OrderID, ProductName → Quantity
 
-PalletDelivery(PalletID, OrderID, DeliveryDate)
-PalletID → OrderID, DeliveryDate
+CREATE TABLE Customers
+(
+Customer VARCHAR(100) NOT NULL PRIMARY KEY,
+Address VARCHAR(101) NOT NULL
+);
 
-Customers(Customer, Address)
+CREATE TABLE Orders
+(
+OrderID INT auto_increment PRIMARY KEY
+Customer VARCHAR(100) REFERENCES Customers(Customer)
+);
+
+CREATE TABLE PartialOrders 
+(
+OrderID INT REFERENCES(Orders(OrderID))
+ProductName VARCHAR(100) REFERENCES Recipes(ProductName) 
+Quantity INT NOT NULL CHECK (Quantity>0),
+PRIMARY KEY (OrderID, ProductName) 
+);
+
+CREATE TABLE PalletDelivery
+(
+PalletID INT REFERENCES Pallets(PalletID) PRIMARY KEY,
+OrderID INT REFERENCES Orders(OrderID),
+DeliveryDate datetime NOT NULL DEFAULT NOW()
+);
